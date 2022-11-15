017e5e7802358fe8c8bf5e04418449372f339361
Haley <hiuyin.leung37@student.qcc.cuny.edu>
2022-11-15 02:07:37 -0500

    Variable update
    Added the card's name into the variable for later checking

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -12,11 +12,14 @@ function App() {
     console.log(props.id);
     if (selection1_id < 0){//Change Selection 1
       selection1_id = props.id
+      selection1_name = props.cardName
     }else if (selection2_id < 0){//Change Selection 2
       if (props.id == selection1_id){
         selection1_id = -1
+        selection1_name = ""
       }else{
         selection2_id = props.id
+        selection2_name = props.cardName
       }
     }else if (selection1_name == selection2_name){//Check if both matches
 
