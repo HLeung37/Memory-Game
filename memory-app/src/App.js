3a554bbff8923649ccc0e21e19e6ca7b7dd37127
Haley <hiuyin.leung37@student.qcc.cuny.edu>
2022-11-14 22:46:35 -0500

    Added 4 cards
    Added card 5 - 8

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Card from './Card'
 
 function App() {
-  const cardValues = ["Card1", "Card2", "Card3", "Card4"];
+  const cardValues = ["Card1", "Card2", "Card3", "Card4", "Card5", "Card6", "Card7", "Card8"];
   return (
     <div className="App">
       {cardValues.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
