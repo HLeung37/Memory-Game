6477e6c8d6954b52ba26d855e3ce32bb57a0582a
Haley <hiuyin.leung37@student.qcc.cuny.edu>
2022-11-14 22:51:30 -0500

    Altered card value
    Made card 5-8, 1-4 to have duplicate values

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Card from './Card'
 
 function App() {
-  const cardValues = ["Card1", "Card2", "Card3", "Card4", "Card5", "Card6", "Card7", "Card8"];
+  const cardValues = ["Card1", "Card2", "Card3", "Card4", "Card1", "Card2", "Card3", "Card4"];
   return (
     <div className="App">
       {cardValues.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
