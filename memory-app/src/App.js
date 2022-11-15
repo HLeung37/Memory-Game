9931f8e4cc32541e3a24f7479fc5de6b29d1c9a3
Haley <hiuyin.leung37@student.qcc.cuny.edu>
2022-11-15 05:44:12 -0500

    Attempt
    Attempting to implement new method of creating card game in the div

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -4,7 +4,8 @@ import Card from './Card'
 function App() {
   var cardValues = ["Card1", "Card2", "Card3", "Card4", "Card1", "Card2", "Card3", "Card4"];
   var selected_item = null;
-  var debounce = false
+  var cards = [];
+  var debounce = false;
 
   function randomize(){
     for (var i = cardValues.length - 1; i > 0; i--) {
@@ -45,14 +46,20 @@ function App() {
       }
     }
   }
+  randomize()
+  var cardList=cardValues.map((item,idx)=>{
+    return <Card id={idx} key={idx} cardText={item} function1={checkFlipped}/>
+  })
+
+  function refreshList(){
+    cardList=cardValues.map((item,idx)=>{
+      return <Card id={idx} key={idx} cardText={item} function1={checkFlipped}/>
+    })
+  }
 
   function createGame(){
-    randomize()
-    const cardGrid = document.querySelector(".card-grid")
-    const cards = cardValues.map((item,idx)=>(<Card id={idx} key={idx} cardText={item} function1={checkFlipped}/>))
-    for (var i = 0;i < cardValues.length;i++){
-      //cardGrid.appendChild(cards[i]);
-    }
+    refreshList()
+    return cardList
   }
 
   function newGame(){
@@ -65,13 +72,11 @@ function App() {
     }
   }
 
-
-  setInterval(createGame(),3000)
   return (
     <div>
     <button className="newGame" onClick={newGame}>New Game</button>
       <div className = "card-grid">
-        {cardValues.map((item,idx)=>(<Card id={idx} key={idx} cardText={item} function1={checkFlipped}/>))}
+        {createGame()}
       </div>
 
     </div>
