2d92adb84b85a83843ffac5d2d7d139bdcccbea7
Haley <hiuyin.leung37@student.qcc.cuny.edu>
2022-11-15 02:01:34 -0500

    Functionality
    Added more functionality to the printFlipped function
    Changed name to checkFlipped

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -3,23 +3,29 @@ import Card from './Card'
 
 function App() {
   const cardValues = ["Card1", "Card2", "Card3", "Card4", "Card1", "Card2", "Card3", "Card4"];
-  const selection1 = "";
-  const selection2 = "";
+  var selection1_id = -1;
+  var selection2_id = -1;
+  var selection1_name = "";
+  var selection2_name = "";
 
-  function printFlipped(props){
-    console.log(props.cardText);
-    if (selection1 == ""){//Change Selection 1
-
-    }else if (selection2 == ""){//Change Selection 2
-
-    }else{//Check if both matches
+  function checkFlipped(props){
+    console.log(props.id);
+    if (selection1_id < 0){//Change Selection 1
+      selection1_id = props.id
+    }else if (selection2_id < 0){//Change Selection 2
+      if (props.id == selection1_id){
+        selection1_id = -1
+      }else{
+        selection2_id = props.id
+      }
+    }else if (selection1_name == selection2_name){//Check if both matches
 
     }
   }
 
   return (
     <div className="App">
-      {cardValues.map((item,idx)=>(<Card key={idx} cardText={item} function1={printFlipped}/>))}
+      {cardValues.map((item,idx)=>(<Card id={idx} key={idx} cardText={item} function1={checkFlipped}/>))}
     </div>
   );
 }
