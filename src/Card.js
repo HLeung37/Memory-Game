cddb00868123c55b896691b39bc8a591aa0596c1
Haley <hiuyin.leung37@student.qcc.cuny.edu>
2022-11-17 10:07:30 -0500

    Reference to card
    Changed references to images

--- src/Card.js
+++ src/Card.js
@@ -36,8 +36,8 @@ class Card extends React.Component {
   }
 
   getImage = () => {
-    console.log("../"+this.state.cardText+".png")
-    return "../"+this.state.cardText+".png"
+    console.log("https://hleung37.github.io/Memory-Game/"+this.state.cardText+".png")
+    return "https://hleung37.github.io/Memory-Game/"+this.state.cardText+".png"
   }
 
   handleClick = () => {
@@ -52,7 +52,7 @@ class Card extends React.Component {
       return (
         <div className = "back"
              onClick={this.handleClick}>
-             <img src = "../GenshinCardBack.png" />
+             <img src = "https://hleung37.github.io/Memory-Game/GenshinCardBack.png" />
         </div>
       );
 		}
